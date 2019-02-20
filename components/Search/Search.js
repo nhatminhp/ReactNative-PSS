ae596cd340f1699d2e433b51f58b3078ba63933a
nguyen <sonnc@deha-soft.com>
2019-02-21 06:54:10 +0700

    push search recipe screen

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { SearchBar } from 'react-native-elements';
-import { Text, View, StyleSheet, Image, KeyboardAvoidingView, ListView, ActivityIndicator, Alert } from 'react-native';
-
+import { Text, View, StyleSheet, Image, KeyboardAvoidingView, ListView, ActivityIndicator, Alert, TouchableOpacity } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
 import { TextField } from 'react-native-material-textfield';
 import { Dropdown } from 'react-native-material-dropdown';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 export default class Search extends React.Component {
 	static navigationOptions = {
 		header: null,
@@ -11,6 +13,8 @@ export default class Search extends React.Component {
 	constructor(props) {
     	super(props);
     	this.state = {
+            SelectedIngredients: [],
+            SelectedCategory: '',
     		keyword: '',
     		category: [{
                 value: 'Appetizer',
@@ -20,36 +24,68 @@ export default class Search extends React.Component {
                 value: 'Desert',
               }],
             ingredients:[{
-                value: 'Egg',
-              }, {
-                value: 'Banana',
-              }, {
-                value: 'Apple',
-              }, {
-                value: 'Flour',
-              }, {
-                value: 'butter',
-              }],
+                          id: 'Egg',
+                          name: 'Egg',
+                        }, {
+                          id: 'Banana',
+                          name: 'Banana',
+                        }, {
+                          id: 'Cauliflower',
+                          name: 'Cauliflower',
+                        }, {
+                          id: 'Cannabis',
+                          name: 'Cannabis',
+                        }, {
+                          id: 'Carrot',
+                          name: 'Carrot',
+                        }, {
+                          id: 'Minced Pork',
+                          name: 'Minced Pork',
+                        }, {
+                          id: 'Mozzarella',
+                          name: 'Mozzarella',
+                        }, {
+                          id: 'Sausage',
+                          name: 'Sausage',
+                        }, {
+                          id: 'hot dog',
+                          name: 'hot dog',
+                        }, {
+                          id: 'wine',
+                          name: 'wine',
+                        }, {
+                          id: 'Chicken',
+                          name: 'Chicken',
+                        }, {
+                          id: 'Bread',
+                          name: 'Bread',
+                        }],
               ingredientsDropdown: [],
+              categorySelected:[],
+              ingredientsSelected: [],
         }
-        this.handleChooseIngredients = this.handleChooseIngredients.bind(this);
+        this.onSelectedIngredientsChange = this.onSelectedIngredientsChange.bind(this);
+        this.test = this.test.bind(this);
     }
-    handleChooseIngredients() {
-        let arr=this.state.ingredientsDropdown;
-        let l1=this.state.ingredientsDropdown.length;
-        arr.push(<Dropdown label='Ingredients' data={this.state.ingredients} onChangeText={()=>this.handleChooseIngredients()}/>);
-        arr.pop();
-        this.setState({ingredientsDropdown: arr})
+    test(SelectedIngredients) {
+        let a='';
+        for(var i=0,j=this.state.SelectedIngredients.length;i<j;i++){
+            a+=this.state.SelectedIngredients[i];
+        }
+        Alert.alert(a+this.state.SelectedCategory+this.state.keyword);
     }
+    onSelectedIngredientsChange(SelectedIngredients) {
+        this.setState({ SelectedIngredients });
+    };
   render() {
-        this.state.ingredientsDropdown.push(<Dropdown label='Ingredients' data={this.state.ingredients} onChangeText={()=>this.handleChooseIngredients()}/>)
     return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <KeyboardAwareScrollView behavior="padding" style={styles.container}>
         <View style={styles.listOptions}>
             <View style={styles.marginTop}></View>
             <View style={[styles.keyword, styles.searchOption]}>
                 <TextField
                     label="Keyword"
+                    baseColor="black"
                     returnKeyType="next"
                     autoCorrect={false}
                     autoCapitalize="none"
@@ -57,17 +93,47 @@ export default class Search extends React.Component {
                 />
             </View>
             <View style={[styles.searchOption]}>
-            <Dropdown
-                label='Category'
-                data={this.state.category}
-            />
+                <Dropdown
+                    label='Category'
+                    baseColor='black'
+                    data={this.state.category}
+                    onChangeText={(SelectedCategory) => this.setState({SelectedCategory})}
+                />
             </View>
-            <View style={[styles.time, styles.searchOption]}>
-                {this.state.ingredientsDropdown}
-            </View>
+            <KeyboardAwareScrollView style={[styles.searchOption]}>
+                <MultiSelect
+                      items={this.state.ingredients}
+                      uniqueKey="id"
+                      ref={(component) => { this.multiSelect = component }}
+                      onSelectedItemsChange={this.onSelectedIngredientsChange}
+                      selectedItems={this.state.SelectedIngredients}
+                      selectText="Select Ingredients"
+                      searchInputPlaceholderText="Search Ingredients..."
+                      onChangeInput={ (text)=> console.log(text)}
+                      altFontFamily="ProximaNova-Light"
+                      tagRemoveIconColor="#CCC"
+                      tagBorderColor="#CCC"
+                      tagTextColor="#CCC"
+                      selectedItemTextColor="#CCC"
+                      selectedItemIconColor="#CCC"
+                      itemTextColor="#000"
+                      displayKey="name"
+                      searchInputStyle={{ color: '#CCC' }}
+                      hideSubmitButton={true}
+                      fontFamily="Slab Serif"
+                      fontSize={17}
+                      textColor="black"
+                />
+                </KeyboardAwareScrollView>
         </View>
-        <View style={styles.searchButton}></View>
-      </KeyboardAvoidingView>
+        <View style={styles.searchButton}>
+            <TouchableOpacity style={styles.button} onPress={()=>this.test()}>
+                <Text>
+                    Search a recipe
+                </Text>
+            </TouchableOpacity>
+        </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
@@ -77,17 +143,28 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	listOptions: {
-	    flex: 9
+	    flex: 5,
+	    height: 400
 	},
 	marginTop: {
 	    height: 50
 	},
 	searchButton: {
-	    flex: 1
+	    flex: 1,
+	    alignItems: 'center'
+	},
+	button: {
+	    backgroundColor: '#52E8FF',
+        padding:20,
+        marginBottom: 50,
+        borderRadius: 50,
+        width: 250,
+        alignItems: 'center'
 	},
 	searchOption: {
-	    height: 40,
-	    margin: 10
+	    height: 55,
+	    margin: 10,
+	    paddingLeft:10,
 	},
 	keyword: {
 //	    backgroundColor: '#e7e7f3'
